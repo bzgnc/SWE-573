c0dd0dd81a2a58e882f25c7c6ffe64f142954bb2
bzgnc <uci235@gmail.com>
2021-06-22 02:27:32 +0300

    final version v1.1

--- medtagger_dashboard/settings.py
+++ medtagger_dashboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-9m56o1=owy3_k0dj!%6bf$74#_$bfznej$qk92dnh##tl!1l-&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
